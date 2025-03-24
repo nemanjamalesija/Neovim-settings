980b4a2dc4e3cd1de28b177ab9a8860005d072ae
Nemanja Malesija <malesija.nemanja@gmail.com>
2025-03-24 15:57:17 +0100

    Create treesitter.lua

---
+++ nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,31 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+      local configs = require("nvim-treesitter.configs")
+
+      configs.setup({
+        ensure_installed = {
+          "lua",
+          "vim",
+          "vimdoc",
+          "query",
+          "php",
+          "html",
+          "css",
+          "scss",
+          "javascript",
+          "typescript",
+          "vue",
+        },
+        sync_install = false,
+        highlight = { enable = true },
+        indent = { enable = true },
+        auto_install = false,
+        modules = {},
+        ignore_install = {},
+      })
+    end,
+  },
+}
