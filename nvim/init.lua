b316ca0afedc745282865e3afe9440580c36b55d
Nemanja Malesija <malesija.nemanja@gmail.com>
2025-03-25 12:06:35 +0100

    Update init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,8 +3,6 @@ vim.g.loaded_netrwPlugin = 1
 
 require("config.lazy")
 
-vim.api.nvim_set_hl(0, "SnacksIndent", { fg = "#5b595c" })
-
 -- Configure diagnostics to only show underlines
 vim.diagnostic.config({
   virtual_text = false,
