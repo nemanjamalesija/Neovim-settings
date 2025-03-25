b3ec11c12ebc2202086ab184d23ca67399a5efb8
Nemanja Malesija <malesija.nemanja@gmail.com>
2025-03-25 12:18:31 +0100

    Create diagnostics.lua

---
+++ nvim/lua/plugins/diagnostics.lua
@@ -0,0 +1,12 @@
+-- Configure diagnostics
+vim.diagnostic.config({
+  underline = false,
+  severity_sort = true,
+})
+
+-- Underline colors
+-- vim.cmd([[
+-- hi DiagnosticUnderlineError guisp='#ff6188' gui=undercurl
+-- hi DiagnosticUnderlineWarn guisp='#fd9621' gui=undercurl
+-- set termguicolors
+-- ]])
