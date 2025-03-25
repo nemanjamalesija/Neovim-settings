33e353708e44409c51448a40543f09819bcade36
Nemanja Malesija <malesija.nemanja@gmail.com>
2025-03-25 12:18:00 +0100

    Update init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,17 +3,3 @@ vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
 require("config.lazy")
-
--- Configure diagnostics
-vim.diagnostic.config({
-  underline = false,
-  severity_sort = true,
-})
-
--- Underline colors
--- vim.cmd([[
--- hi DiagnosticUnderlineError guisp='#ff6188' gui=undercurl
--- hi DiagnosticUnderlineWarn guisp='#fd9621' gui=undercurl
--- set termguicolors
--- ]])
---
