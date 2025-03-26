ea84d5a74bb3683ca9a27375d1cd4dd2f74c9a6d
Nemanja Malesija <malesija.nemanja@gmail.com>
2025-03-26 23:36:04 +0100

    Update telescope.lua

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -3,18 +3,37 @@ return {
   dependencies = {
     "nvim-lua/plenary.nvim",
     { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-    "nvim-tree/nvim-web-devicons",
+    "nvim-tree/nvim-tree.lua",
   },
   config = function()
     local telescope = require("telescope")
-    -- local actions = require("telescope.actions")
-    -- local transform_mod = require("telescope.actions.mt").transform_mod
+    local actions = require("telescope.actions")
+    local action_state = require("telescope.actions.state")
+    local nvim_tree_api = require("nvim-tree.api")
+
+    -- Fast sync: Skip `vim.schedule` and directly reveal the file
+    -- local sync_fast = function(prompt_bufnr)
+    --   actions.select_default(prompt_bufnr) -- Select file in Telescope
+    --   local selected = action_state.get_selected_entry()
+    --   if not selected then
+    --     return
+    --   end
+
+    --   -- Get absolute path (works for most pickers: find_files, git_files, etc.)
+    --   local file_path = selected.path or (selected.cwd and selected.cwd .. "/" .. selected.value) or selected.value
+    --
+    --   -- Immediately reveal in Nvim-Tree (no delay)
+    --   if nvim_tree_api.tree.is_visible() then
+    --     nvim_tree_api.tree.find_file(file_path)
+    --   else
+    --     nvim_tree_api.tree.toggle({ focus = false, find_file = true, path = file_path })
+    --   end
+    -- end
 
     telescope.setup({
       defaults = {
+        file_ignore_patterns = { "node_modules" },
         layout_strategy = "vertical",
-        path_display = { "smart" },
-        mappings = {},
         vimgrep_arguments = {
           "rg",
           "--color=never",
