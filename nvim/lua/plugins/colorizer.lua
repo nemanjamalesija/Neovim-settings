ff262ba1f10e18f37310e26a306e3e1727728f53
Nemanja Malesija <malesija.nemanja@gmail.com>
2025-03-26 23:36:27 +0100

    Create colorizer.lua

---
+++ nvim/lua/plugins/colorizer.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "norcalli/nvim-colorizer.lua",
+    config = function()
+      require("colorizer").setup({
+        "css",
+        "scss",
+        "javascript",
+        "html",
+      })
+    end,
+  },
+}
