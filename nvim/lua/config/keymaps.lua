33af3d4d0edb198e9be30706dda78f13c6589779
Nemanja Malesija <malesija.nemanja@gmail.com>
2025-03-29 10:18:56 +0100

    Update keymaps.lua

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -3,7 +3,7 @@
 -- Add any additional keymaps here
 -- -- Add this to ~/.config/nvim/lua/config/keymaps.lua (if using LazyVim)
 -- or add to your init.lua if you have a different setup
---
+
 -- File explorer
 vim.keymap.set("n", "<leader>e", "<cmd>NvimTreeFindFileToggle<cr>", { desc = "Toggle NvimTree (reveal file)" })
 vim.keymap.set("n", "<leader>o", "<cmd>NvimTreeFindFile<cr>", { desc = "Reveal current file in NvimTree" })
@@ -17,9 +17,12 @@ vim.api.nvim_set_keymap("t", "<leader>`", [[<C-\><C-n><cmd>ToggleTerm<CR>]], { n
 vim.keymap.set("n", "<leader>s", "<cmd>w<cr>", { desc = "Save file" })
 vim.keymap.set("n", "<leader>q", "<cmd>q<cr>", { desc = "Quit buffer" })
 
--- Telescope
+-- Telescope/files
 vim.keymap.set("n", "<leader><leader>", "<cmd>Telescope find_files<cr>", { desc = "Find files" })
 vim.keymap.set("n", "<leader>F", "<cmd>Telescope live_grep<cr>", { desc = "Live grep (text)" })
+
+vim.keymap.set("n", "<leader>f", "/\\c", { noremap = true }, { desc = "Find in current buffer" })
+vim.keymap.set("n", "<leader>D", ":%s/", { noremap = true }, { desct = "Substitute word" })
 
 -- LSP
 vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "Hover documentation" })
@@ -28,11 +31,13 @@ vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, { desc = "Find referen
 vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Code actions" })
 
 -- Misc
-vim.keymap.set("i", "<S-CR>", "<cmd><Esc>o<cr>", { desc = "New line below" })
+vim.api.nvim_set_keymap("i", "<M-CR>", "<Esc>o", { noremap = true, silent = true })
 
 -- Bufferline move left/right
-vim.api.nvim_set_keymap("n", "<M-h>", ":BufferLineMovePrev<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<M-l>", ":BufferLineMoveNext<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<M-h>", ":bprevious<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<M-l>", ":bnext<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<C-M-h>", ":BufferLineMovePrev<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<C-M-l>", ":BufferLineMoveNext<CR>", { noremap = true, silent = true })
 
 -- Close buffer
 vim.api.nvim_set_keymap("n", "<M-w>", ":bdelete<CR>", { noremap = true, silent = true })
