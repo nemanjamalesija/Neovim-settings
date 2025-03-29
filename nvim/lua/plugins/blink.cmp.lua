417a531b10d527aa745e13c4bf43fee8d264ffcd
Nemanja Malesija <malesija.nemanja@gmail.com>
2025-03-29 10:21:05 +0100

    Update blink.cmp.lua

--- nvim/lua/plugins/blink.cmp.lua
+++ nvim/lua/plugins/blink.cmp.lua
@@ -229,7 +229,7 @@ return {
       },
       -- Displays a preview of the selected item on the current line
       ghost_text = {
-        enabled = true,
+        enabled = false,
       },
     }
 
