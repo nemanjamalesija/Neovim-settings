f394f69a18f50ac00723b33b76adcc34ae587ded
Nemanja Malesija <malesija.nemanja@gmail.com>
2025-03-30 03:03:30 +0200

    Create fugitive.lua

---
+++ nvim/lua/plugins/fugitive.lua
@@ -0,0 +1,9 @@
+return {
+  "tpope/vim-fugitive",
+  event = "VeryLazy",
+  config = function()
+    vim.keymap.set("n", "<leader>gs", ":Git status<CR>", { desc = "[G]it [S]tatus" })
+    vim.keymap.set("n", "<leader>gd", ":Gdiff<CR>", { desc = "[G]it [D]iff" })
+    vim.keymap.set("n", "<leader>gb", ":Git blame<CR>", { desc = "[G]it [B]lame" })
+  end,
+}
