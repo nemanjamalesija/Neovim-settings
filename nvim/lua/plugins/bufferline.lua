c095994044b44b1b409ff40f71d002b0e505a08a
Nemanja Malesija <malesija.nemanja@gmail.com>
2025-04-02 15:59:24 +0200

    Update bufferline.lua

--- nvim/lua/plugins/bufferline.lua
+++ nvim/lua/plugins/bufferline.lua
@@ -6,51 +6,33 @@ return {
     config = function()
       require("bufferline").setup({
         options = {
-          mode = "buffers", -- set to "tabs" to only show tabpages instead
-          style_preset = require("bufferline").style_preset.bufferline, -- or bufferline.style_preset.minimal,
-          buffer_close_icon = "󰅖",
+          mode = "buffers",
+          style_preset = require("bufferline").style_preset.bufferline,
           modified_icon = "● ",
-          close_icon = " ",
-          left_trunc_marker = " ",
-          right_trunc_marker = " ",
           tab_size = 18,
           diagnostics = false,
+          mappings = true,
+          separator_style = "slope",
           offsets = {
             {
               filetype = "NvimTree",
               text = "File Explorer",
               text_align = "center",
-              separator = true,
+              separator = false,
             },
           },
           show_buffer_icons = false,
           show_buffer_close_icons = false,
           show_tab_indicators = true,
-          --[[   separator_style = "slope", ]]
           always_show_bufferline = true,
         },
-
-        highlights = {
-          fill = {
-            bg = "#fdf6e3",
-          },
-          -- buffer_selected = {
-          --   fg = "#ffcc00",
-          --   bold = true,
-          --   italic = false,
-          -- },
-        },
+        -- highlights = {
+        --   fill = {
+        --     bg = "#fdf6e3",
+        --     fg = "#fdf6e3",
+        --   },
+        -- },
       })
-
-      -- Alternative method using `vim.cmd`
-      -- vim.cmd([[
-      --   hi BufferLineModified guifg=#fd9621 gui=underline
-      --   hi BufferLineModifiedVisible guifg=#fd9621 gui=underline
-      --   hi BufferLineModifiedSelected guifg=#fd9621 gui=underline
-      -- ]])
-    end,
-  },
-}
     end,
   },
 }
