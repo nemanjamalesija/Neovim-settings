5650fc6ba1a7a6bb38aa0ef8ea29fcbf0aa12a19
Nemanja Malesija <malesija.nemanja@gmail.com>
2025-04-13 15:58:57 +0200

    Update mini.lua

--- nvim/lua/plugins/mini.lua
+++ nvim/lua/plugins/mini.lua
@@ -2,5 +2,5 @@ return {
   { "echasnovski/mini.pairs", enabled = false },
   { "echasnovski/mini.animate", opts = { cursor = { enable = false } } },
   { "echasnovski/mini.icons", enabled = false },
-  { "echasnovski/mini.bufremove" },
+  { "echasnovski/mini.bufremove", version = false },
 }
