0ebac38bd9546eb2c758098f25f5a2af1e028a5b
Nemanja Malesija <malesija.nemanja@gmail.com>
2025-04-23 22:38:16 +0200

    Update lsp.lua

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,131 +1,138 @@
 return {
-  { "williamboman/mason.nvim" },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    config = function()
-      require("mason-lspconfig").setup({
-        ensure_installed = {
-          "ts_ls",
-          "volar",
-          "intelephense",
-          "html",
-          "cssls",
-          "tailwindcss",
-          "somesass_ls",
-          "lua_ls",
-          "emmet_ls",
-          "stylelint_lsp",
-        },
-        automatic_installation = true,
-      })
-    end,
-  },
-  {
-    "neovim/nvim-lspconfig",
-    opts = {
-      inlay_hints = { enabled = false },
-      document_highlight = {
-        enabled = false,
-      },
+    { "williamboman/mason.nvim" },
+    {
+        "williamboman/mason-lspconfig.nvim",
+        config = function()
+            vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
+                border = "rounded",
+                max_width = 80,
+                max_height = 20,
+            })
+
+            require("mason-lspconfig").setup({
+                ensure_installed = {
+                    "ts_ls",
+                    "volar",
+                    "intelephense",
+                    "html",
+                    "cssls",
+                    "tailwindcss",
+                    "somesass_ls",
+                    "lua_ls",
+                    "emmet_ls",
+                    "stylelint_lsp",
+                },
+                automatic_installation = true,
+            })
+        end,
     },
-    config = function()
-      local lspconfig = require("lspconfig")
+    {
+        "neovim/nvim-lspconfig",
+        opts = {
+            inlay_hints = { enabled = false },
+            document_highlight = {
+                enabled = false,
+            },
+        },
+        config = function()
+            local lspconfig = require("lspconfig")
 
-      local capabilities = vim.lsp.protocol.make_client_capabilities()
-      capabilities.textDocument.completion.completionItem.snippetSupport = true
+            local capabilities = vim.lsp.protocol.make_client_capabilities()
+            capabilities.textDocument.completion.completionItem.snippetSupport = true
 
-      -- Eslint
-      lspconfig.eslint.setup({
-        capabilities = capabilities,
-        on_attach = function(client, bufnr)
-          vim.api.nvim_create_autocmd("BufWritePre", {
-            buffer = bufnr,
-            callback = function()
-              vim.cmd("EslintFixAll")
-            end,
-          })
-        end,
-      })
-
-      -- Stylelint
-      lspconfig.stylelint_lsp.setup({
-        capabilities = capabilities,
-        on_attach = function(client, bufnr)
-          vim.api.nvim_create_autocmd("BufWritePre", {
-            buffer = bufnr,
-            callback = function()
-              vim.lsp.buf.format({
-                filter = function(client)
-                  return client.name == "stylelint_lsp"
+            -- Eslint
+            lspconfig.eslint.setup({
+                capabilities = capabilities,
+                on_attach = function(client, bufnr)
+                    vim.api.nvim_create_autocmd("BufWritePre", {
+                        buffer = bufnr,
+                        callback = function()
+                            vim.cmd("EslintFixAll")
+                        end,
+                    })
                 end,
-                async = false,
-              })
-            end,
-          })
+            })
+
+            -- Stylelint
+            lspconfig.stylelint_lsp.setup({
+                capabilities = capabilities,
+                on_attach = function(client, bufnr)
+                    vim.api.nvim_create_autocmd("BufWritePre", {
+                        buffer = bufnr,
+                        callback = function()
+                            vim.lsp.buf.format({
+                                filter = function(client)
+                                    return client.name == "stylelint_lsp"
+                                end,
+                                async = false,
+                            })
+                        end,
+                    })
+                end,
+                settings = {
+                    stylelintplus = {
+                        autoFixOnSave = true,
+                        autoFixOnFormat = true,
+                    },
+                },
+                filetypes = { "css", "scss", "less", "sass" },
+            })
+
+            -- PHP
+            lspconfig.intelephense.setup({
+                capabilities = capabilities,
+                filetypes = { "php", "view", "template" }, -- Add your custom filetypes
+                settings = {
+                    intelephense = {
+                        files = {
+                            associations = { "*.php", "*.view", "*.template" }, -- Associate files
+                        },
+                    },
+                },
+            })
+
+            -- Typescript LSP server
+            --[[  lspconfig.ts_ls.setup({}) ]]
+
+            -- Volar LSP server
+            lspconfig.volar.setup({
+                capabilities = capabilities,
+                filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
+                init_options = {
+                    vue = {
+                        hybridMode = false,
+                    },
+                },
+            })
+
+            -- CSS LSP server
+            lspconfig.cssls.setup({
+                capabilities = capabilities,
+                settings = {
+                    css = {
+                        validate = true,
+                        completion = {
+                            completePropertyWithSemicolon = true,
+                            triggerPropertyValueCompletion = true,
+                        },
+                    },
+                    scss = {
+                        validate = true,
+                        completion = {
+                            completePropertyWithSemicolon = true,
+                            triggerPropertyValueCompletion = true,
+                        },
+                    },
+                    less = {
+                        validate = true,
+                        completion = {
+                            completePropertyWithSemicolon = true,
+                            triggerPropertyValueCompletion = true,
+                        },
+                    },
+                },
+            })
         end,
-        settings = {
-          stylelintplus = {
-            autoFixOnSave = true,
-            autoFixOnFormat = true,
-          },
-        },
-        filetypes = { "css", "scss", "less", "sass" },
-      })
-
-      -- PHP
-      lspconfig.intelephense.setup({
-        capabilities = capabilities,
-        filetypes = { "php", "view", "template" }, -- Add your custom filetypes
-        settings = {
-          intelephense = {
-            files = {
-              associations = { "*.php", "*.view", "*.template" }, -- Associate files
-            },
-          },
-        },
-      })
-
-      -- Typescript LSP server
-      lspconfig.ts_ls.setup({})
-
-      -- Volar LSP server
-      lspconfig.volar.setup({
-        capabilities = capabilities,
-        filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
-        init_options = {
-          vue = {
-            hybridMode = false,
-          },
-        },
-      })
-
-      -- CSS LSP server
-      lspconfig.cssls.setup({
-        capabilities = capabilities,
-        settings = {
-          css = {
-            validate = true,
-            completion = {
-              completePropertyWithSemicolon = true,
-              triggerPropertyValueCompletion = true,
-            },
-          },
-          scss = {
-            validate = true,
-            completion = {
-              completePropertyWithSemicolon = true,
-              triggerPropertyValueCompletion = true,
-            },
-          },
-          less = {
-            validate = true,
-            completion = {
-              completePropertyWithSemicolon = true,
-              triggerPropertyValueCompletion = true,
-            },
-          },
-        },
-      })
-    end,
-  },
+    },
 }
+
