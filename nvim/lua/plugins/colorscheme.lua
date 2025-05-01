6ea3ef72a74f93531cf188d869b5b704577b66cb
Nemanja Malesija <malesija.nemanja@gmail.com>
2025-05-01 12:19:41 +0200

    Update colorscheme.lua

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -58,16 +58,16 @@ return {
                     local ref_bg = "#dcdcdc"
 
                     highlights.String = { fg = "#2aa198" }
-                    highlights["@variable"] = { fg = "fg_text" }
-                    highlights["@variable.member"] = { fg = "fg_text" }
+                    highlights["@variable"] = { fg = fg_text }
+                    highlights["@variable.member"] = { fg = fg_text }
                     highlights["@variable.builtin"] = { fg = "#ff005f" }
 
                     highlights.NormalFloat = { bg = bg_poup }
-                    highlights.FloatBorder = { bg = bg_poup, fg = "fg_text" }
+                    highlights.FloatBorder = { bg = bg_poup, fg = fg_text }
                     highlights.CursorLine = { bg = "#ececec" }
 
-                    highlights.TelescopeNormal = { bg = bg_poup, fg = "fg_text" }
-                    highlights.TelescopeBorder = { bg = bg_poup, fg = "fg_text" }
+                    highlights.TelescopeNormal = { bg = bg_poup, fg = fg_text }
+                    highlights.TelescopeBorder = { bg = bg_poup, fg = fg_text }
 
                     highlights.DiagnosticVirtualTextError = { fg = colors.error, bg = "NONE" }
                     highlights.DiagnosticVirtualTextWarn = { fg = colors.warning, bg = "NONE" }
