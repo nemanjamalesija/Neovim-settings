12e6e0dafe33c3cfddb2a7b1ec21972a3ed33932
Nemanja Malesija <malesija.nemanja@gmail.com>
2025-08-12 11:09:59 +0200

    Update noice.lua

--- nvim/lua/plugins/noice.lua
+++ nvim/lua/plugins/noice.lua
@@ -3,6 +3,15 @@ return {
     event = "VeryLazy",
     dependencies = {
         "MunifTanjim/nui.nvim",
+        {
+            "rcarriga/nvim-notify",
+            config = function()
+                require("notify").setup({
+                    stages = "fade",
+                    
+                })
+            end,
+        },
     },
     opts = {
         lsp = {
@@ -13,9 +22,50 @@ return {
         },
         presets = {
             command_palette = true,
-            long_message_to_split = true,
+            long_message_to_split = false,
             inc_rename = true,
             lsp_doc_border = false,
+        },
+        notify = {
+            enabled = true,
+            view = "notify",
+        },
+        views = {
+            notify = {
+                backend = "notify",
+                fallback = "mini",
+                format = "notify",
+                replace = false,
+                merge = false,
+                win_options = {
+                    winblend = 0,
+                    wrap = true,
+                },
+                size = {
+                    width = "auto",
+                    height = "auto",
+                },
+                position = {
+                    row = 1,
+                    col = -2,
+                },
+            },
+        },
+        routes = {
+            {
+                filter = {
+                    event = "notify",
+                },
+                view = "notify",
+                opts = {
+                    timeout = 3000,
+                    title = "",
+                    position = {
+                        row = 1,
+                        col = -2,
+                    },
+                },
+            },
         },
     },
 }
